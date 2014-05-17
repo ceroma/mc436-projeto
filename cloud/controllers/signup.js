d6b0fb70599550c8aa60051345acb4572a544d0a
Carlos Eduardo Rosa Machado <carlos.rmachado@ymail.com>
2014-05-17 15:47:17 -0300

    Facebook Registration Plugin
    Using <fb:registration /> as sign up form, allowing users to prefill the form with their Facebook information.

--- cloud/controllers/signup.js
+++ cloud/controllers/signup.js
@@ -1,18 +1,40 @@
+var Buffer = require('buffer').Buffer;
+var crypto = require('crypto');
+
+// Fields to be passed to <fb:registration />
+var FORM_FIELDS = JSON.stringify(
+  [
+    {'name': 'name'},
+    {'name': 'first_name'},
+    {'name': 'last_name'},
+    {'name': 'username', 'description': 'Nome de Usuário', 'type': 'text'},
+    {'name': 'password'},
+    {'name': 'captcha'}
+  ]
+).replace(/\"/g, "'");
+
 // Renders Sign Up Screen
 exports.index = function(req, res) {
-  res.render('signup');
+  res.render('signup', { form_fields : FORM_FIELDS });
 };
 
 // Creates New User
 exports.createNewUser = function(req, res) {
-  var newUser = new Parse.User();
-  newUser.set("firstName", req.body.firstname);
-  newUser.set("lastName", req.body.lastname);
-  newUser.set("username", req.body.username);
-  newUser.set("password", req.body.password);
+  // Parse signed request
+  var registration_data = getRegistrationData(req.body.signed_request);
+  if (!registration_data) {
+    res.redirect('/signup');
+    return;
+  }
+
+  var new_user = new Parse.User();
+  new_user.set("firstName", registration_data.first_name);
+  new_user.set("lastName", registration_data.last_name);
+  new_user.set("username", registration_data.username);
+  new_user.set("password", registration_data.password);
 
   // Create new user
-  newUser.signUp(null, {
+  new_user.signUp(null, {
     success: function(user) {
       // Say hello to new user
       res.render('hello', { message: 'Hello, ' + user.get("firstName") });
@@ -22,4 +44,52 @@ exports.createNewUser = function(req, res) {
       res.redirect('/signup');
     }
   });
+};
+
+// Parses the signed request received from Facebook and returns the actual
+// form data if all security checks pass, otherwise returns null.
+function getRegistrationData(signed_request) {
+  var request_data = signed_request.split('.', 2);
+  var encoded_payload = request_data[1];
+  var encoded_signature = request_data[0];
+
+  if (!isFacebookSignatureValid(encoded_signature, encoded_payload)) {
+    return null
+  }
+
+  // Decode payload
+  var payload = JSON.parse(new Buffer(encoded_payload, 'base64'));
+  if (!isFacebookPayloadValid(payload)) {
+    return null;
+  }
+
+  return payload.registration;
+};
+
+// Validates request signature
+function isFacebookSignatureValid(encoded_signature, encoded_payload) {
+  var secret = 'FACEBOOK_APP_SECRET';
+  var hmac = crypto.createHmac('sha256', secret);
+  var expected_signature =
+    hmac.update(encoded_payload).digest('base64')
+    .replace(/\+/g, '-').replace(/\//g, '_').replace('=', '');
+  return encoded_signature == expected_signature;
+};
+
+// Validates registration data
+function isFacebookPayloadValid(payload) {
+  if (!payload || !payload.registration) {
+    return false;
+  }
+
+  if (payload.algorithm != 'HMAC-SHA256') {
+    return false;
+  }
+
+  if (!payload.registration_metadata ||
+      FORM_FIELDS != payload.registration_metadata.fields) {
+    return false;
+  }
+
+  return true;
 };
