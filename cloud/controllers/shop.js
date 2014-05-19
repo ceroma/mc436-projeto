be24773b17b0b2e7133c4d676e36faf8b0e815e6
Carlos Eduardo Rosa Machado <carlos.rmachado@ymail.com>
2014-05-18 23:57:12 -0300

    Cart Reminder
    If user has a pending cart, show a reminder about it on main screen.

--- cloud/controllers/shop.js
+++ cloud/controllers/shop.js
@@ -1,3 +1,12 @@
+var Cart = Parse.Object.extend('Cart', {
+  isVisible: function() {
+    // Can't see cart after 1h without buying it
+    var one_hour_in_ms = 60 * 60 * 1000;
+    var now = (new Date()).getTime();
+    var creation_time = (new Date(this.createdAt)).getTime();
+    return (now - creation_time) < one_hour_in_ms;
+  }
+});
 var Product = Parse.Object.extend('Product'); 
 
 // Renders the shop if user is logged in
@@ -12,10 +21,26 @@ exports.index = function(req, res) {
   Parse.User.current().fetch().then(function(user) {
     current_user = user;
 
+    // Fetch last cart, so we can remind user
+    var last_cart = user.get('lastCart');
+    if (last_cart) {
+      return last_cart.fetch();
+    } else {
+      return Parse.Promise.as();
+    }
+
+  }).then(function() {
+    // Fetch all available products
     var query = new Parse.Query(Product);
     return query.find();
+
   }).then(function(products) {
-    res.render('shop', { user : current_user, products : products });
+    var last_cart = current_user.get('lastCart');
+    res.render('shop', {
+      user : current_user,
+      products : products,
+      pending_cart : (last_cart && last_cart.isVisible()) ? last_cart : null
+    });
   }, function(error) {
     res.send(500, 'Failed loading list of products');
   });
