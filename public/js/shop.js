35749316372c071a2e9af28189e8dff586d34fd5
Carlos Eduardo Rosa Machado <carlos.rmachado@ymail.com>
2014-05-26 11:48:46 -0300

    Speech Recognition
    Simple speech recognition to populate the search field on supported browsers.

--- public/js/shop.js
+++ public/js/shop.js
@@ -4,7 +4,7 @@ function dismissReminder() {
     reminder.remove();
   }
   return false;
-}
+};
 
 function addItem(button_element) {
   var input_field = getInputField(button_element);
@@ -26,4 +26,42 @@ function getInputField(button_element) {
   var input_group = button_element.parentNode.parentNode;
   var input_field = input_group.getElementsByClassName('input-sm')[0];
   return input_field;
+};
+
+function setUpSpeechRecognition() {
+  if (!('webkitSpeechRecognition' in window)) {
+    return false;
+  }
+
+  var search_field = document.getElementsByName('q')[0];
+  var microphone_icon = document.getElementsByClassName('microphone-icon')[0];
+
+  var recognition = new webkitSpeechRecognition();
+  recognition.onstart = function() {
+    addClass(microphone_icon, 'recording');
+  };
+  recognition.onend = function() {
+    removeClass(microphone_icon, 'recording');
+  };
+  recognition.onresult = function(results) {
+    if (results.results && results.results[0] && results.results[0][0]) {
+      search_field.value = results.results[0][0].transcript;
+    }
+  };
+
+  microphone_icon.onclick = function() {
+    recognition.start();
+  };
+
+  removeClass(microphone_icon, 'hidden');
+  search_field.style.paddingRight = '44px';
+  return false;
+};
+
+function addClass(element, class_name) {
+  element.className = element.className + ' ' + class_name;
+};
+
+function removeClass(element, class_name) {
+  element.className = element.className.replace(new RegExp(class_name), '');
 };
